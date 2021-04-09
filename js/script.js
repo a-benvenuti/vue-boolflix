5b0547a52caabe711548218e85d2abe68583684c
Andrea <b.a.benvenuti3@gmail.com>
2021-04-09 15:16:17 +0200

    completata Milestone 1

--- js/script.js
+++ js/script.js
@@ -3,15 +3,25 @@ var app = new Vue(
     el: '#root',
     // -------------------------------------------------------------------------
     data: {
-
+      films: [],
+      ricercaUtente: '',
     },
     // -------------------------------------------------------------------------
     methods: {
+      search: function() {
+            axios.get('https://api.themoviedb.org/3/search/movie?', {
+                params: {
+                    api_key: "acdbcdcacc7acfa06c08eabb7258ad40",
+                    query: this.ricercaUtente,
+                    language: "it-IT",
+                }
+            })
+            .then((risposta) => {
+              this.films = risposta.data.results;
+            })
 
-    },
-    // -------------------------------------------------------------------------
-    mounted: function() {
-
+            this.ricercaUtente = '';
+        }
     },
     // -------------------------------------------------------------------------
   }
