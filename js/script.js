ccd1d31f0e8f467ef71a46ec109ff5e16afb9cc3
Andrea <b.a.benvenuti3@gmail.com>
2021-04-09 16:39:37 +0200

    aggiunti film al caricamento della pagina + aggiunto logo

--- js/script.js
+++ js/script.js
@@ -4,12 +4,14 @@ var app = new Vue(
     // -------------------------------------------------------------------------
     data: {
       films: [],
+      filmsPopular: [],
       ricercaUtente: '',
     },
     // -------------------------------------------------------------------------
     methods: {
+      // funzione per la ricerca del film da parte dell'utente
       search: function() {
-            axios.get('https://api.themoviedb.org/3/search/movie?', {
+            axios.get('https://api.themoviedb.org/3/search/movie', {
                 params: {
                     api_key: "acdbcdcacc7acfa06c08eabb7258ad40",
                     query: this.ricercaUtente,
@@ -22,6 +24,19 @@ var app = new Vue(
 
             this.ricercaUtente = '';
         }
+    },
+    // -------------------------------------------------------------------------
+    mounted: function() {
+        // chiamata per i film piu visti
+        axios.get('https://api.themoviedb.org/3/movie/popular', {
+          params: {
+              api_key: "acdbcdcacc7acfa06c08eabb7258ad40",
+              language: "it-IT",
+          }
+        })
+        .then((risposta) => {
+          this.filmsPopular = risposta.data.results;
+        })
     },
     // -------------------------------------------------------------------------
   }
